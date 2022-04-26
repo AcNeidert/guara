557e0b4ddd377c6a16521677a839cc47bc8c9edb
André Neidert <acneidert@gmail.com>
2022-04-26 15:35:49 -0300

    Create packages

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import GInput from './src/components/input/GInput';
-import GButton from './src/components/button/GButton';
-
-export default {
-    GInput,
-    GButton
-}
+export * as components from './packages/components'
---
+++ packages/components/index.js
@@ -0,0 +1,7 @@
+import GInput from './input/GInput'
+import GButton from './button/GButton'
+
+export default {
+    GInput,
+    GButton
+}
